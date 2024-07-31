54904fcec941f4a75a3a819138d7bb8bc74527a8
swapnali-pise <swapnali.pise@cryptextechnologies.com>
2024-07-31 16:36:00 +0530

    first commit

---
+++ app/controllers/api/v1/surveys_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class SurveysController < ApplicationController
+      protect_from_forgery with: :null_session # Disable CSRF protection for API requests
+
+      def create
+        @survey = Survey.new(survey_params)
+        if @survey.save
+          render json: { status: 'SUCCESS', message: 'Survey created', data: @survey }, status: :created
+        else
+          render json: { status: 'ERROR', message: 'Survey not created', data: @survey.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def index
+        @surveys = Survey.all
+        render json: { status: 'SUCCESS', message: 'Loaded all surveys', data: @surveys }, status: :ok
+      end
+
+      def update
+        @survey = Survey.find(params[:id])
+        if @survey.update(survey_params)
+          render json: { status: 'SUCCESS', message: 'Survey updated', data: @survey }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Survey not updated', data: @survey.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def survey_params
+        params.require(:survey).permit(:name, :description)
+      end
+    end
+  end
+end
